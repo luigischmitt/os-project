b6cc269d23a78cfa6230b332ca9b9c6194507282
Kevin Gabriel <kevinmangueira@gmail.com>
2026-02-10 17:13:01 -0300

    Adição da parte do código em C do capítulo 3

--- src/boot.asm
+++ src/boot.asm
@@ -8,16 +8,32 @@ align 4
     dd 0x00000000            ; flags (0 = mínimo)
     dd -(0x1BADB002 + 0x00000000) ; checksum
 
+; --- Stack para o kernel ---
+KERNEL_STACK_SIZE equ 4096                  ; tamanho da stack em bytes
+
+SECTION .bss
+align 4                                      ; alinhamento de 4 bytes
+kernel_stack:                                ; label aponta para o início da memória reservada
+    resb KERNEL_STACK_SIZE                   ; reserva stack para o kernel
+
 SECTION .text
 global _start
-extern kernel_main
+extern sum_of_three                          ; funcao definida em outro lugar
 
 _start:
-    ; "Hello Cafebabe": coloca 0xCAFEBABE no EAX e trava.
+    ; Inicializa stack do kernel
+    mov esp, kernel_stack + KERNEL_STACK_SIZE  ; aponta ESP para o topo da stack
+
+    ; "Hello Cafebabe": coloca 0xCAFEBABE no EAX
     mov eax, 0xCAFEBABE
 
-    ; chama o kernel em C (só pra provar o pipeline)
-    call kernel_main
+    ; --- Chamada de exemplo para sum_of_three ---
+    push dword 3            ; argumento 3
+    push dword 2            ; argumento 2
+    push dword 1            ; argumento 1
+    call sum_of_three       ; chama a função
+    add esp, 12             ; limpa os argumentos da stack (3 * 4 bytes)
+    ; resultado estará em EAX
 
 .hang:
     cli
